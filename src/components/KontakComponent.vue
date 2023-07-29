b611859db0b53def5f6540cce6224e0e6ca77823
devinferdyand <devinferdynand00@gmail.com>
2023-07-29 12:27:16 +0700

    update

--- src/components/KontakComponent.vue
+++ src/components/KontakComponent.vue
@@ -40,6 +40,10 @@ import { Input } from 'flowbite-vue'
         >
           HUBUNGI VIA INSTAGRAM
         </a>
+
+        <a href="mailto:jobsdevin2@gmail.com" target="_blank"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">email</a>
+
         <br />
       </figure>
     </div>
